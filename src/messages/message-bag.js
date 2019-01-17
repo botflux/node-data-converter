5b4189746d94f6a49175f357717e1106ffccf34f
Victor Mendele <victor.mendele68@gmail.com>
2019-01-17 10:48:20 +0100

    fix: comments message-bag

--- src/messages/message-bag.js
+++ src/messages/message-bag.js
@@ -1,7 +1,19 @@
+/**
+ * An error
+ */
 const MESSAGE_ERROR = 'error'
+/**
+ * A warning
+ */
 const MESSAGE_WARNING = 'warning'
+/**
+ * A notice
+ */
 const MESSAGE_NOTICE = 'notice'
 
+/**
+ * Message type colors
+ */
 const messageStyles = {
     [MESSAGE_ERROR]: `\x1b[31m`,
     [MESSAGE_WARNING]: "\x1b[33m",
@@ -12,15 +24,30 @@ const messageStyles = {
  * Represents a message
  */
 class Message {
+    /**
+     * Initializes a new Message using an options object containing a type and a text.
+     * 
+     * @param {{}} options 
+     */
     constructor ({ type = MESSAGE_ERROR, text = "Something went wrong" }) {
         this.type = type
         this.text = text
     }    
 
+    /**
+     * Returns the text of this message.
+     * 
+     * @returns {String} Message text
+     */
     getText () {
         return this.text
     }
 
+    /**
+     * Returns the type of this message
+     * 
+     * @returns {String} Message type
+     */
     getType () {
         return this.type
     }
@@ -30,14 +57,25 @@ class Message {
  * Handle all messages
  */
 class MessageBag {
+    /**
+     * Initializes a new instance of MessageBag
+     */
     constructor () {
         this.messages = []
     }
 
+    /**
+     * Add a new Message to the message bag.
+     * 
+     * @param {Message} message Message instance
+     */
     addMessage (message) {
         this.messages = [...this.messages, ...[message]]
     }
 
+    /**
+     * Shows all messages added to this message bag.
+     */
     showMessages () {
         this.messages.forEach((message) => {
             console.log(`${messageStyles[message.getType()]} ${message.getType()}: ${message.getText()}`)
@@ -45,8 +83,16 @@ class MessageBag {
     }
 }
 
+/**
+ * Application message bag instance
+ */
 let messageBag = null
 
+/**
+ * Returns the current MessageBag instance.
+ * 
+ * @returns {MessageBag} MessageBag instance
+ */
 const getMessageBag = () => {
     if (messageBag === null) messageBag = new MessageBag()
     return messageBag 
@@ -55,6 +101,7 @@ const getMessageBag = () => {
 module.exports = {
     getMessageBag,
     Message,
+    MessageBag,
     MESSAGE_ERROR,
     MESSAGE_NOTICE,
     MESSAGE_WARNING
